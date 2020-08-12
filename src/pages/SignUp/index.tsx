331bcb22485c16d6c8494dac15252992e7ecdbed
matheus1519 <1519matheus@gmail.com>
2020-08-12 04:12:28 -0300

    Estrutura inicial, rotas, padrões de estilos e outros

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>SignUP</Text>
+    </Container>
+  );
+};
+
+export default SignUp;
